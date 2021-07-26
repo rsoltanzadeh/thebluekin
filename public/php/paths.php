4daa29f38edbae873c9b126bc7f32be8b8a1e63d
Ramin <null>
2021-07-26 20:06:18 +0200

    change api directory name from 'php' to 'api'

--- public/php/paths.php
+++
@@ -1,8 +0,0 @@
-<?php
-$URL_base = "https://luparabianca.com";
-$sensitive_data = "/home/ramin/sensitive_data";
-$login_page = $URL_base;
-$register_page = $URL_base . "/introduce/";
-$home_page = $URL_base . "/home/";
-$lobby_page = $URL_base . "/lobby/";
-?>
